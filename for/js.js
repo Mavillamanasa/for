6ce51de248d44c557dd129bbc6f90364b12e046f
mavillamanasa2002@gmail.com <mavillamanasa2002@gmail.com>
2024-11-02 14:49:22 +0530

    first commit

---
+++ for/js.js
@@ -0,0 +1,37 @@
+//for loop
+for(var a=1;a<=10;a++){
+    console.log(a);
+}
+for(var b=11;b>6;b--){
+    console.log(b);
+
+}
+for(var c=24;c>=15;c--){
+    console.log(c);
+}
+
+//while loop
+var a1=1;
+while(a1<=7){
+    console.log(a1)
+    a1++
+}
+
+var i=1
+while(i<4){
+    i++
+}
+console.log(i)
+
+//do while 
+var m=1;
+do{
+    m++
+    console.log(m);
+}while(m<=5)
+
+var n=10;
+do{
+    n--
+    console.log(n);
+}while(n==7)
